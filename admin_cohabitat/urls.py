686b38041b3389f540cbff6c2bd963eb70e414ad
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-09-21 14:04:25 -0300

    fix login structure

--- admin_cohabitat/urls.py
+++ admin_cohabitat/urls.py
@@ -8,8 +8,9 @@ from . import views
 
 urlpatterns = [
                   url(r'^login', auth_views.login, name='login'),
-                  url(r'^logout', views.login, name='logout'),
-                  url(r'^home', views.home, name='logout'),
+                  url(r'^logout', auth_views.logout, {'next_page': '/admin'}, name='logout'),
+                  url(r'^home', views.home, name='home'),
+                  url(r'^$', auth_views.login, name='login'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
