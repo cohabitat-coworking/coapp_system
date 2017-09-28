68cc4437f6e7bbc57a4f699d44799741c82da3f4
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-09-28 12:16:41 -0300

    add routing capabilities

--- admin_cohabitat/views/config.py
+++ admin_cohabitat/views/config.py
@@ -1,14 +1,20 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
 from backend import models
 from backend.models import ContactInfo, Coworking
 
 
-def config(request):
-    coworking = Coworking.objects.all().first()
+# admin/confing/{coworking_id}
+def config(request, coworking_id):
+    try:
+        coworking = Coworking.objects.get(pk=coworking_id)
+    except ObjectDoesNotExist:
+        return render(request, 'not_found.html', status=status.HTTP_404_NOT_FOUND)
+
     contact_infos = ContactInfo.objects.all()
-
-
     data = {'coworking_info': coworking, 'contact_infos': contact_infos}
     return render(
         request,
